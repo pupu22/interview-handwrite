34b822fb5a42c330fea4a57a5fd9de1f3a3ed8e6
灼灼 <45594835+pupu22@users.noreply.github.com>
2023-08-04 13:32:57 +0800

    Update Promise.js

--- src/Promise.js
+++ src/Promise.js
@@ -1,35 +1,68 @@
-const PENDING = Symbol()
-const FULLFILLED = Symbol()
-const REJECTED = Symbol()
+[面试官：“你能手写一个 Promise 吗” ] https://zhuanlan.zhihu.com/p/183801144
+                                          
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
 
-const MyPromise = function(fn) {
-  this.state = PENDING
-  this.value = ''
+class Promise {
+  constructor(exectuor){
+    this.status = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+    // 存放成功的回调
+    this.onResolvedCallbacks = [];
+    // 存放失败的回调
+    this.onRejectedCallbacks= [];
 
-  const resolve = (value) => {
-    this.state = FULLFILLED;
-    this.value = value;
-  }
+    let resolve = (value) => {
+      if(this.status === PENDING){
+        this.status = FULFILLED
+        this.value = value
+        this.onResolvedCallbacks.forEach(fn=>fn());
+      }
+    }
+    let reject = (reason) => {
+      if(this.status === PENDING){
+        this.status = REJECTED
+        this.reason = reason
+        this.onRejectedCallbacks.forEach(fn=>fn())
+      }
+    }
 
-  const reject = (error) => {
-    this.state = REJECTED;
-    this.value = error;
-  }
-
-  this.then = (onFullfilled, onRejected) => {
-    if(this.state === FULLFILLED){
-      onFullfilled(this.value)
-    } else {
-      onRejected(this.value)
+    try{
+      exectuor(resolve, reject)
+    }catch(error){
+      reject(error)
     }
   }
 
-  // 执行传入的函数
-  try {
-    fn(resolve, reject)
-  } catch (error){
-    reject(error)
+  then(onFulfilled, onRejected){
+    if(this.status === FULFILLED){
+      onFulfilled(this.value)
+    }
+    if(this.status === REJECTED){
+      onRejected(this.reason)
+    }
+    if(this.status === PENDING){
+      this.onResolvedCallbacks.push(()=>{
+        onFulfilled(this.value)
+      })
+      this.onRejectedCallbacks.push(()=>{
+        onRejected(this.value)
+      })
+    }
   }
 }
 
-let 
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('成功');
+  },1000);
+}).then(
+  (data) => {
+    console.log('success', data)
+  },
+  (err) => {
+    console.log('faild', err)
+  }
+)
